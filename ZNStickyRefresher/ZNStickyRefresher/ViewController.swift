e8ea055f386297bce0f7f5d3a39050671db14c9c
FunctionMaker <FunctionMaker@foxmail.com>
2017-09-22 20:18:58 +0800

    init repository.

---
+++ ZNStickyRefresher/ZNStickyRefresher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZNStickyRefresher
+//
+//  Created by FunctionMaker on 2017/9/22.
+//  Copyright © 2017年 FunctionMaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
