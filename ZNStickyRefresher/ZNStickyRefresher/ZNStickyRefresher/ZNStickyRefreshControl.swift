adb1751248c1e8b7b9478eea78665f98e68f40a7
FunctionMaker <FunctionMaker@foxmail.com>
2017-09-22 21:29:59 +0800

    add refresh view to refresh control.

--- ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefreshControl.swift
+++ ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefreshControl.swift
@@ -10,10 +10,44 @@ import UIKit
 
 /// finish refreshing control logic
 class ZNStickyRefreshControl: UIControl {
+    
+    /// refresher's container view
+    private weak var scrollView: UIScrollView?
+    
+    /// refresh view
+    private lazy var refreshView = ZNStickyRefreshView.refreshView()
+    
+    // MARK: - constructor
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func willMove(toSuperview newSuperview: UIView?) {
+        super.willMove(toSuperview: newSuperview)
+        
+        guard let newSuperview = newSuperview as? UIScrollView else { return }
+        
+        scrollView = newSuperview
+        
+        // let `self` observe newSuperview's contentOffset
+        newSuperview.addObserver(self, forKeyPath: "contentOffset", options: [], context: nil)
+    }
+    
+    // FIXME: - KVO call-back
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+    }
+    
+    // FIXME: - start refreshing
     func beginRefreshing() {
         
     }
     
+    // FIXME: - finish refreshing
     func endRefreshing() {
         
     }
--- ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefreshView.swift
+++ ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefreshView.swift
@@ -10,5 +10,12 @@ import UIKit
 
 /// display refresh effect
 class ZNStickyRefreshView: UIView {
-
+    /// return a instance of ZNStickyRefreshView
+    ///
+    /// - Returns: a instance load from nib file
+    class func refreshView() -> ZNStickyRefreshView {
+        let nib = UINib(nibName: "ZNStickyRefreshView", bundle: nil)
+        
+        return nib.instantiate(withOwner: nil, options: nil)[0] as! ZNStickyRefreshView
+    }
 }
