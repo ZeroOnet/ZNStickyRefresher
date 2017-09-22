f36bf1ed2e7f47c2565d3b4ac12fb6e9a0e019b8
FunctionMaker <FunctionMaker@foxmail.com>
2017-09-22 21:04:50 +0800

    init fundamental component.

---
+++ ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefreshControl.swift
@@ -0,0 +1,20 @@
+//
+//  ZNStickyRefreshControl.swift
+//  ZNStickyRefresher
+//
+//  Created by FunctionMaker on 2017/9/22.
+//  Copyright © 2017年 FunctionMaker. All rights reserved.
+//
+
+import UIKit
+
+/// finish refreshing control logic
+class ZNStickyRefreshControl: UIControl {
+    func beginRefreshing() {
+        
+    }
+    
+    func endRefreshing() {
+        
+    }
+}
---
+++ ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefresher/ZNStickyRefreshView.swift
@@ -0,0 +1,14 @@
+//
+//  ZNStickyRefreshView.swift
+//  ZNStickyRefresher
+//
+//  Created by FunctionMaker on 2017/9/22.
+//  Copyright © 2017年 FunctionMaker. All rights reserved.
+//
+
+import UIKit
+
+/// display refresh effect
+class ZNStickyRefreshView: UIView {
+
+}
